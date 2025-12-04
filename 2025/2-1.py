1daca642c5028a14ccc6bf1a60b5a5d1523c16c4
Eric Stefanko <erics@Erics-MacBook-Air.local>
2025-12-04 17:12:01 -0500

    Update

--- 2025/2-1.py
+++ 2025/2-1.py
@@ -2,7 +2,7 @@
 
 #total_ids_to_check = "9226466333-9226692707,55432-96230,4151-6365,686836-836582,519296-634281,355894-471980,971626-1037744,25107-44804,15139904-15163735,155452-255998,2093-4136,829776608-829880425,4444385616-4444502989,2208288-2231858,261-399,66-119,91876508-91956018,2828255673-2828317078,312330-341840,6464-10967,5489467-5621638,1-18,426-834,3434321102-3434378477,4865070-4972019,54475091-54592515,147-257,48664376-48836792,45-61,1183-1877,24-43"
 
-total_ids_to_check = "24-43"
+total_ids_to_check = "22-44"
 
 total_ids_to_check_split = total_ids_to_check.split(",")
 
@@ -34,7 +34,27 @@ for entry in total_ids_to_check_split:
         number_string_character_array = list(number_string)
 
         #Compare the first character index with the middle character index and increase each iteration
-        middle_number = int(len(number_string_character_array)/2)
+
+        x = 0 #while loop counter
+        initial_index_number = 0
+        comparison_equidistant_number = int(len(number_string_character_array)/2)
+
+        while x < len(number_string_character_array)//2:
+
+            if number_string_character_array[initial_index_number] == number_string_character_array[comparison_equidistant_number]:
+                initial_index_number += 1
+                comparison_equidistant_number += 1
+                x += 1
+                is_made_of_duplicate_numbers_status = True
+            else:
+                is_made_of_duplicate_numbers_status = False
+                break
+        
+        #Add numbers if the number to be evaluated is made of duplicate numbers
+        if is_made_of_duplicate_numbers_status == True:
+            duplicate_values += number
+
+print(duplicate_values)
 
 
 
